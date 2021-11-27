8654576d0d8b31384ed30a2728e6bc37b04f63a8
bitcubix <bitcubix@bitcubix.io>
2021-11-27 13:43:15 +0100

    add AndroidProducts.mk to add lunch options

---
+++ AndroidProducts.mk
@@ -0,0 +1,9 @@
+# Add the OP515BL1 to lunch build options
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_OP515BL1.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_OP515BL1-user \
+    lineage_OP515BL1-userdebug \
+    lineage_OP515BL1-eng
