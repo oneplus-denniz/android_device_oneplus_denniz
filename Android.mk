3d062f84ce157e80bf51cb9733acb45b2dd50386
bitcubix <bitcubix@bitcubix.io>
2021-11-27 17:29:54 +0100

    add releasetools and obsolete partion table

--- Android.mk
+++ Android.mk
@@ -4,8 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),OP515BL1)
 
-#TODO: add releasetools
-#$(call add-radio-file,releasetools/dynamic-remove-oneplus)
+$(call add-radio-file,releasetools/dynamic-remove-oneplus)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
