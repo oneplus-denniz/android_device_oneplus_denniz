ae0971d935cd7fbefe57d7c26acdf4405af22d7a
bitcubix <bitcubix@bitcubix.io>
2021-11-30 23:45:25 +0100

    update releasetools

--- Android.mk
+++ Android.mk
@@ -1,10 +1,16 @@
-# master makefile for android build
+############################################
+#										   #
+# Copyright (C) 2021 The LineageOS Project #
+#										   #
+# SPDX-License-Identifier: Apache-2.0	   #
+#										   #
+############################################
 
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),OP515BL1)
 
-$(call add-radio-file,releasetools/dynamic-remove-oneplus)
+$(call add-radio-file,releasetools/dynamic-remove-oplus)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
